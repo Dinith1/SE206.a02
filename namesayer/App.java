e0a6c5bbca446a50c8511c8ee73e04296d6c5b2b
dinith1 <wannigamadinith@gmail.com>
2018-08-18 19:03:26 +1200

    did some more stuff

--- namesayer/App.java
+++ namesayer/App.java
@@ -17,10 +17,16 @@ import javafx.scene.control.TextArea;
 import javafx.collections.*;
 import javafx.util.*;
 import javafx.scene.media.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.stage.Modality;
 
 public class App extends Application {
 
+    Media pick;
     MediaPlayer player;
+    MediaView mediaView;
+    String listSelected;
 
     public static void main(String[] args) {
         launch(args);
@@ -44,17 +50,26 @@ public class App extends Application {
         ObservableList<String> items = populateList();
         creationList.setItems(items);
 
+        //listView.getSelectionModel().getSelectedItem();
+        creationList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                System.out.println("ListView selection changed from oldValue = " + oldValue + " to newValue = " + newValue);
+                listSelected = newValue;
+            }
+        });
 
 
-        Media pick = new Media(getClass().getResource("BigBuckBunny_320x180.mp4").toExternalForm());
+
+        pick = new Media(getClass().getResource("creations/BigBuckBunny_320x180.mp4").toExternalForm());
         player = new MediaPlayer(pick);
-        MediaView mediaView = new MediaView(player);
+        mediaView = new MediaView(player);
         
 
 
 
         //scrollList.setPrefSize(120, 120);
-        Rectangle rect = new Rectangle(50, 50, Color.RED);
+        //Rectangle rect = new Rectangle(50, 50, Color.RED);
         creationBox.getChildren().addAll(creationList, mediaView);
 
 
@@ -64,7 +79,7 @@ public class App extends Application {
 
         Button createBtn = new Button("create");
         createBtn.setMaxWidth(Double.MAX_VALUE);
-        createBtn.setOnAction(e -> createCreation());
+        createBtn.setOnAction(e -> createCreation(primaryStage));
         
         Button deleteBtn = new Button("delete");
         deleteBtn.setMaxWidth(Double.MAX_VALUE);
@@ -86,7 +101,7 @@ public class App extends Application {
         ObservableList<String> list = FXCollections.observableArrayList();;
 		
 		try {
-			ProcessBuilder builder = new ProcessBuilder("ls");
+			ProcessBuilder builder = new ProcessBuilder("ls", "creations/");
 			Process process = builder.start();
 
 			InputStream stdout = process.getInputStream();
@@ -115,13 +130,35 @@ public class App extends Application {
 
 
     private void playCreation() {
+        pick = new Media(getClass().getResource("creations/"+listSelected).toExternalForm());
+        player = new MediaPlayer(pick);
+        mediaView.setMediaPlayer(player);
         player.play();
     }
 
-    private void createCreation() {
-        String testvideo = "testing12345";
-        doCommand("ffmpeg", "-f", "lavfi", "-i", "color=c=blue:s=320x240:d=5", "-vf", "drawtext=fontfile=/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf:fontsize=30: fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='"+testvideo+"'", "_video22345.mp4"/*, "&> /dev/null"*/);
+    private void createCreation(Stage primaryStage) {
+
+        Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner(primaryStage);
+
+        TextField enterName = new TextField();
+        Button confirmBtn = new Button("Confirm");
+        Button cancelBtn = new Button("Cancel");
+
+        VBox dialogVbox = new VBox(20);
+        dialogVbox.getChildren().addAll(new Label("Enter a name"), enterName, confirmBtn, cancelBtn);
+
+        Scene dialogScene = new Scene(dialogVbox, 300, 200);
+        dialog.setScene(dialogScene);
+        dialog.show();
+
+
+        String name = "memes";
+
+        doCommand("ffmpeg", "-f", "lavfi", "-i", "color=c=blue:s=320x240:d=5", "-vf", "drawtext=fontfile=/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf:fontsize=30: fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='"+name+"'", "creations/memes.mp4"/*, "&> /dev/null"*/);
         System.out.println("CREATED!");
+        recordAudio();
     }
 
     private void deleteCreation() {
@@ -130,6 +167,10 @@ public class App extends Application {
 
     private void doCommand(String... cmd) {
         try {
+            // Remove pre-existing temporary files if they exist
+            ProcessBuilder removeBuilder = new ProcessBuilder("rm", "creations/_audio.wav", "creations/_video.mp4");
+            Process removeProcess = removeBuilder.start();
+
 			ProcessBuilder builder = new ProcessBuilder(cmd);
 			Process process = builder.start();
 
@@ -153,6 +194,11 @@ public class App extends Application {
 			ex.printStackTrace();
 			System.out.println(cmd + " ***************TRY CATCH FAILED");
 		}
+    }
+
+
+    private void recordAudio() {
+
     }
 
 }
