a9a4cfcedb7a36a38de9c586f3aea0e1361e899d
dinith1 <wannigamadinith@gmail.com>
2018-08-19 14:11:20 +1200

    need to add audio recording

--- namesayer/App.java
+++ namesayer/App.java
@@ -20,6 +20,7 @@ import javafx.scene.media.*;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.stage.Modality;
+import javafx.scene.Node;
 
 public class App extends Application {
 
@@ -27,6 +28,8 @@ public class App extends Application {
     MediaPlayer player;
     MediaView mediaView;
     String listSelected;
+    Double btnWidth = 90.0, btnHeight = 45.0;
+    Stage createWindow = new Stage();
 
     public static void main(String[] args) {
         launch(args);
@@ -46,46 +49,45 @@ public class App extends Application {
         HBox buttonBox = new HBox(30);
         buttonBox.setAlignment(Pos.CENTER);
 
+     
+        Button playBtn = new Button("play");
+        playBtn.setMaxWidth(Double.MAX_VALUE);
+        playBtn.setPrefSize(btnWidth, btnHeight);
+        playBtn.setOnAction(e -> playCreation());
+        playBtn.setDisable(true);
+
+        Button createBtn = new Button("create");
+        createBtn.setMaxWidth(Double.MAX_VALUE);
+        createBtn.setPrefSize(btnWidth, btnHeight);
+        createBtn.setOnAction(e -> createCreation(primaryStage));
+        
+        Button deleteBtn = new Button("delete");
+        deleteBtn.setMaxWidth(Double.MAX_VALUE);
+        deleteBtn.setPrefSize(btnWidth, btnHeight);
+        deleteBtn.setOnAction(e -> deleteCreation());;
+        deleteBtn.setDisable(true);
+
+        buttonBox.getChildren().addAll(playBtn, createBtn, deleteBtn);
+
         ListView<String> creationList = new ListView<String>();
         ObservableList<String> items = populateList();
         creationList.setItems(items);
 
-        //listView.getSelectionModel().getSelectedItem();
         creationList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
                 System.out.println("ListView selection changed from oldValue = " + oldValue + " to newValue = " + newValue);
+                playBtn.setDisable(false);
+                deleteBtn.setDisable(false);
                 listSelected = newValue;
             }
         });
-
-
 
         pick = new Media(getClass().getResource("creations/BigBuckBunny_320x180.mp4").toExternalForm());
         player = new MediaPlayer(pick);
         mediaView = new MediaView(player);
         
-
-
-
-        //scrollList.setPrefSize(120, 120);
-        //Rectangle rect = new Rectangle(50, 50, Color.RED);
         creationBox.getChildren().addAll(creationList, mediaView);
-
-
-        Button playBtn = new Button("play");
-        playBtn.setMaxWidth(Double.MAX_VALUE);
-        playBtn.setOnAction(e -> playCreation());
-
-        Button createBtn = new Button("create");
-        createBtn.setMaxWidth(Double.MAX_VALUE);
-        createBtn.setOnAction(e -> createCreation(primaryStage));
-        
-        Button deleteBtn = new Button("delete");
-        deleteBtn.setMaxWidth(Double.MAX_VALUE);
-        deleteBtn.setOnAction(e -> deleteCreation());;
-
-        buttonBox.getChildren().addAll(playBtn, createBtn, deleteBtn);
 
         base.getChildren().addAll(creationBox, buttonBox);
 
@@ -93,6 +95,9 @@ public class App extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("NameSayer");
         primaryStage.show();
+
+        createWindow.initModality(Modality.APPLICATION_MODAL);
+        createWindow.initOwner(primaryStage);
 
     } 
 
@@ -138,37 +143,99 @@ public class App extends Application {
 
     private void createCreation(Stage primaryStage) {
 
-        Stage dialog = new Stage();
-        dialog.initModality(Modality.APPLICATION_MODAL);
-        dialog.initOwner(primaryStage);
-
+        Label promptMessage = new Label("Enter a name");
         TextField enterName = new TextField();
+        enterName.setPrefSize(100, 20);
+
+
         Button confirmBtn = new Button("Confirm");
+        confirmBtn.setPrefSize(btnWidth, btnHeight);
+        confirmBtn.setOnAction(e -> {
+            String creationName = enterName.getText().toLowerCase();
+            if (checkName(creationName)) {
+                startCreating(creationName);
+            }
+            else {
+                promptMessage.setText("Please enter valid name (only letters, digits, underscores and hyphens)");
+            }
+        });
+        confirmBtn.setPrefSize(btnWidth, btnHeight);
+
         Button cancelBtn = new Button("Cancel");
+        cancelBtn.setPrefSize(btnWidth, btnHeight);
+        cancelBtn.setOnAction(e -> createWindow.hide());
+
+        HBox btnBox = new HBox(30);
+        btnBox.getChildren().addAll(cancelBtn, confirmBtn);
+        btnBox.setAlignment(Pos.CENTER);
 
         VBox dialogVbox = new VBox(20);
-        dialogVbox.getChildren().addAll(new Label("Enter a name"), enterName, confirmBtn, cancelBtn);
+        dialogVbox.getChildren().addAll(promptMessage, enterName, btnBox);
 
-        Scene dialogScene = new Scene(dialogVbox, 300, 200);
-        dialog.setScene(dialogScene);
-        dialog.show();
+        Scene dialogScene = new Scene(dialogVbox, 600, 150);
+        createWindow.setScene(dialogScene);
+        createWindow.setTitle("New Creation");
+        createWindow.show();
 
-
-        String name = "memes";
-
-        doCommand("ffmpeg", "-f", "lavfi", "-i", "color=c=blue:s=320x240:d=5", "-vf", "drawtext=fontfile=/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf:fontsize=30: fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='"+name+"'", "creations/memes.mp4"/*, "&> /dev/null"*/);
-        System.out.println("CREATED!");
-        recordAudio();
     }
+
+    private void startCreating(String name) {
+        doCommand("ffmpeg", "-f", "lavfi", "-i", "color=c=blue:s=320x240:d=5", "-vf", "drawtext=fontfile=/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf:fontsize=30: fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='"+name+"'", name+".mp4"/*, "&> /dev/null"*/);
+        System.out.println("CREATED!");
+        promptToRecord(name);
+    }
+
+    
+    private void promptToRecord(String name) {
+        Label promptMessage = new Label("Click to start recording yourself saying \"" + name + "\" for 5 seconds");
+    
+        Button recordBtn = new Button("Record");
+        recordBtn.setPrefSize(btnWidth, btnHeight);
+        recordBtn.setOnAction(e -> recordAudio());
+        recordBtn.setPrefSize(btnWidth, btnHeight);
+
+        Button cancelBtn = new Button("Cancel");
+        cancelBtn.setPrefSize(btnWidth, btnHeight);
+        cancelBtn.setOnAction(e -> createWindow.hide());
+
+        HBox btnBox = new HBox(30);
+        btnBox.getChildren().addAll(cancelBtn, recordBtn);
+        btnBox.setAlignment(Pos.CENTER);
+
+        VBox dialogVbox = new VBox(20);
+        dialogVbox.getChildren().addAll(promptMessage, btnBox);
+
+        Scene dialogScene = new Scene(dialogVbox, 600, 150);
+        createWindow.setScene(dialogScene);
+        createWindow.setTitle("New Creation");
+        createWindow.show();
+    }
+
+
+    private void recordAudio() {
+        doCommand("ffmpeg", "-f", "alsa", "-i", "default", "-t", "5", "_audio.wav" /*&> /dev/null*/);
+    }
+
+    private boolean checkName(String name) {
+        if (name.matches("^[a-zA-Z0-9 _-]+$")) {
+            return true;
+        } 
+        else {
+            return false;
+        }
+    }
+
 
     private void deleteCreation() {
-
+        player.stop();
     }
-
+    
+    
+    
     private void doCommand(String... cmd) {
         try {
             // Remove pre-existing temporary files if they exist
-            ProcessBuilder removeBuilder = new ProcessBuilder("rm", "creations/_audio.wav", "creations/_video.mp4");
+            ProcessBuilder removeBuilder = new ProcessBuilder("rm", "_audio.wav", "_video.mp4");
             Process removeProcess = removeBuilder.start();
 
 			ProcessBuilder builder = new ProcessBuilder(cmd);
@@ -197,8 +264,6 @@ public class App extends Application {
     }
 
 
-    private void recordAudio() {
-
-    }
+    
 
 }
